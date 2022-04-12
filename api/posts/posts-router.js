5e6dc748260c085cf6f86e826be35dfc7b0511cb
Cannen Hanzik <cannenhanzik2@gmail.com>
2022-04-12 15:48:53 -0500

    created getters, posts, and deletes

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -2,4 +2,29 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+   
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/:id', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.get('/:id/messages', (req, res) => {
+
+})
+
+
 module.exports = router;
